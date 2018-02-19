60dc9dd54cd351b1cd6f7593dda15a056b67822a
Monirul Hasan <kmhasan@gmail.com>
2018-02-19 12:50:39 +0600

    lab 3

--- Bank Account Demo/src/bank/account/demo/BankAccountDemo.java
+++ Bank Account Demo/src/bank/account/demo/BankAccountDemo.java
@@ -18,6 +18,7 @@ public class BankAccountDemo {
         BankAccount b1 = new BankAccount(2331, "Rajon Bardhan", "Moghbazar", 5000);
         BankAccount b2 = new BankAccount(6587, "Md. Ashiqur Rahman", "Notun Bazaar", 6000);
         CurrentAccount b3 = new CurrentAccount(8741, "Md. Hasan Tareque", "Shantinagar", 1000);
+        CreditCard b4 = new CreditCard(4200, "Md. Rakibul Hasan", "Uttara", 50000);
         
         b1.deposit(5000);
         b1.withdraw(1000);
@@ -28,9 +29,16 @@ public class BankAccountDemo {
         b3.deposit(1000);
         b3.withdraw(500);
         
+        b4.withdraw(500);
+        b4.withdraw(10000);
+        b4.withdraw(20000);
+        b4.deposit(500);
+        b4.withdraw(20000);
+        
         b1.print();
         b2.print();
         b3.print();
+        b4.print();
     }
     
 }
