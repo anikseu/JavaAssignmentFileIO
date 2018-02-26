973cc4fa8207063a87fa6318940cf344ce9182ec
Monirul Hasan <kmhasan@gmail.com>
2018-02-26 12:45:35 +0600

    lab 3

--- Basic Calculator/src/basic/calculator/FXMLDocumentController.java
+++ Basic Calculator/src/basic/calculator/FXMLDocumentController.java
@@ -11,25 +11,45 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  *
  * @author kmhasan
  */
 public class FXMLDocumentController implements Initializable {
-    
+
     @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+    private TextField displayTextField;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
+    @FXML
+    private void handleOneAction(ActionEvent event) {
+        String oldValue = displayTextField.getText();
+        String newValue = oldValue.concat("1");
+//        String newValue = oldValue + "1";
+        displayTextField.setText(newValue);
+    }
+
+    @FXML
+    private void handleTwoAction(ActionEvent event) {
+        String oldValue = displayTextField.getText();
+        String newValue = oldValue + "2";
+        displayTextField.setText(newValue);
+    }
+
+    @FXML
+    private void handleZeroAction(ActionEvent event) {
+        // Homework: add appropriate logic (if/else)
+        // to check if there are any leading zeros
+        // Hint: use startsWith method from the String class
+        String oldValue = displayTextField.getText();
+        String newValue = oldValue + "0";
+        displayTextField.setText(newValue);        
+    }
+
 }
